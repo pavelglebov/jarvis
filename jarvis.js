44f594f2409e834b13b02e778e36a7f284966f85
pavelglebov <pg.incloud@gmail.com>
2018-12-02 00:03:24 +0300

    Convert indents to spaces

--- jarvis.js
+++ jarvis.js
@@ -3,48 +3,48 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/client/index.html');
+  res.sendFile(__dirname + '/client/index.html');
 });
 
 var route = {
-	'Initial': function(msg, socket) {
-		if (msg.toLowerCase() == 'привет' ||
-			msg.toLowerCase() == 'hello' ||
-			msg.toLowerCase() == 'hi') {
+  'Initial': function(msg, socket) {
+    if (msg.toLowerCase() == 'привет' ||
+      msg.toLowerCase() == 'hello' ||
+      msg.toLowerCase() == 'hi') {
 
-			socket.emit('new response', 'Hi! My name is Jarvis.');
-			setTimeout(function(){
-				socket.emit('new response', 'This is my initial state with not much logic.');
-			}, 2000);
-		}
-	}
+      socket.emit('new response', 'Hi! My name is Jarvis.');
+      setTimeout(function(){
+        socket.emit('new response', 'This is my initial state with not much logic.');
+      }, 2000);
+    }
+  }
 };
 
 var STATE = 'Initial';
 
 var jarvis = [
-	"Джарвис - это я",
-	"Да да",
-	"Мм?",
-	"Я помогу вам",
-	"Джжаааааарвииииииисссс",
-	"",
-	"",
-	"Я вас слушаю",
-	"Джа-джа-джарвис-джаарвиис",
-	"http://cs624025.vk.me/v624025130/1ffbe/DdYgLRzsr5A.jpg"
+  "Джарвис - это я",
+  "Да да",
+  "Мм?",
+  "Я помогу вам",
+  "Джжаааааарвииииииисссс",
+  "",
+  "",
+  "Я вас слушаю",
+  "Джа-джа-джарвис-джаарвиис",
+  "http://cs624025.vk.me/v624025130/1ffbe/DdYgLRzsr5A.jpg"
 ];
 
 io.on('connection', function(socket) {
-		socket.on('new message', function(msg){
-			if (msg.toLowerCase() == 'jarvis' ||
-				msg.toLowerCase() == 'джарвис') {
-				socket.emit('new jarvis', jarvis[Math.floor(Math.random() * (jarvis.length+1))]);
-			}
-			route[STATE](msg, socket);
-		});
-	});
+    socket.on('new message', function(msg){
+      if (msg.toLowerCase() == 'jarvis' ||
+        msg.toLowerCase() == 'джарвис') {
+        socket.emit('new jarvis', jarvis[Math.floor(Math.random() * (jarvis.length+1))]);
+      }
+      route[STATE](msg, socket);
+    });
+  });
 
 http.listen(3000, function(){
-	console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
